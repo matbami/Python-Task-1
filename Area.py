efe8ff8c856c65dfd3f70c3fc90ae7167c2b55e8
Ayobami <adelekeayobami13@gmail.com>
2020-03-24 20:11:29 +0100

    first commit

---
+++ Area.py
@@ -0,0 +1,6 @@
+#First Python Task
+import math
+print('Calculate the value of your Area')
+radius = float (input('Kindly enter the value of radius: '))
+Area =  math.pi * radius* radius
+print(Area)
